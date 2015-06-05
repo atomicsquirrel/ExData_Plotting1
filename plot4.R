c2624a900448624ac20090871d484cb8984e766c
atomicsquirrel <graupelschauer@yahoo.com>
2015-06-05 09:48:27 -0700

    update

--- plot4.R
+++ plot4.R
@@ -58,7 +58,7 @@ png(file="plot4.png",width = 480, height = 480, units = "px")
 	points(data$DateTime,data$Sub_metering_3,type="l",col="blue")
 	legend("topright", lwd=1,
 		col=c("black","red","blue"),
-		legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+		legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty = "n")
 	# Plot 3 - upper right
 	plot(data$DateTime,data$Voltage,
 		"l",
